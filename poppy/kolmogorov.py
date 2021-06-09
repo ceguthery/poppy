814717594e228c652aba295ff875d1acce906a7a
ceguthery <ceguthery@gmail.com>
2021-06-08 17:48:54 -0700

    Adding Kolmogorov wavefront module
    Input phase size, returns a 3-D array with two phase screens of that size using a Roddier basis description for Kolmogorov turbulence

---
+++ poppy/kolmogorov.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Aug 31 11:31:50 2020
+
+@author: Charlotte E. Guthery
+
+"""
+def kolmogorov(s):
+    #phase=kolmogorov(s)
+    # Returns a random Kolmogorov phase screen of dimension s x s computed 
+    # from the FT of random complex numbers with appropriate amplitudes. 
+    # Screens are computed on a grid of size 2s, with a s x s piece cut out. 
+    # This helps overcome the problem with this techniqe of under-representing 
+    # tilt. Tandom tilts are also explicity added to give a reasonable 
+    # approximation of the overall Kolmogorav structure function on all scales.
+    
+    import numpy as np
+    import matplotlib.pyplot as plt
+    
+    ph=np.zeros([s,s,2]) #initialize phase variable
+    [x,y]=np.meshgrid(range(-s,s),range(-s,s))
+    r=np.sqrt(np.multiply(x,x)+np.multiply(y,y)) #make a radial ordinate
+    
+    f1=np.random.randn(2*s,2*s) #make two sets of gaussian random numbers
+    f2=np.random.randn(2*s,2*s)
+
+    f=f1+1j*f2 #turn the two random numbers into a complex random number
+    ps=np.power(r,-11/6) #Kolomogorov power spectrum amplitude
+    ps[s][s]=0; #setting the centr of the power spectrum (DC term) to zero
+    
+    scale=15.2 #this number makes the tilts work out 
+
+    xt=np.random.randn(2)*(scale/s) #random amplitudes for tip and tilt to 
+    yt=np.random.randn(2)*(scale/s) #mitigate power spectral rolloff at low frequencies 
+
+    #This has Kolmogorov phases in real and imaginary parts
+    sc= np.fft.fft2(np.fft.fftshift(np.multiply(np.multiply(np.abs(f),ps),np.exp(1j*np.angle(f))))) 
+
+    #seperating the real and imaginary parts
+    ph[:,:,0]=np.real(sc[0:s,0:s])+xt[0]*x[0:s,0:s]+yt[0]*y[0:s,0:s]
+    ph[:,:,1]=np.real(sc[0:s,0:s])+xt[1]*x[0:s,0:s]+yt[1]*y[0:s,0:s]
+    
+    #make the mean phase zero
+    ph[:,:,0]=ph[:,:,0]-np.mean(ph[:,:,0])
+    ph[:,:,1]=ph[:,:,1]-np.mean(ph[:,:,1])
+    
+    #pick just one of the two phase screens for present purposes scale to 
+    #something that will give reasonable ansewrs on a 1024 grid with 256 pupil
+    ph=ph[:,:,1]*3
+    plt.imshow(ph)
+    np.var(np.var(ph))
+    return ph 
